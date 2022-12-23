9e64728f612f4485c099d36e02c5b3ce6abf3ec1
prehyejin <prehyejin28@gmail.com>
2022-12-23 21:20:13 +0900

    add cors option

--- mysql-server.js
+++ mysql-server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv").config({ path: ".env" });
 //const fileupload = require('express-fileupload');
 
@@ -8,8 +8,29 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const allowedOrigins = [
+	"http://localhost:3000",
+	"http://ict.gatc.co.kr",
+	"https://ict.gatc.co.kr",
+];
+
 // Set CORS option
-app.use(cors());
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			// allow requests with no origin
+			// (like mobile apps or curl requests)
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.indexOf(origin) === -1) {
+				var msg =
+					"The CORS policy for this site does not " +
+					"allow access from the specified Origin.";
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		},
+	})
+);
 
 // Parse requests of content-type: application/json
 app.use(bodyParser.json());
@@ -21,21 +42,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(fileupload());
 
 // RESTful API route for DB
-app.use('/', require('./src/app/mysql/route/route.js'));
+app.use("/", require("./src/app/mysql/route/route.js"));
 
 // static files
-app.use('/static', express.static('public'));
+app.use("/static", express.static("public"));
 
 // DB Connection
-const db = require('./src/app/mysql/model/index.js');
-      db.sequelizeConfig.sync();
+const db = require("./src/app/mysql/model/index.js");
+db.sequelizeConfig.sync();
 
 // Default route for server status
-app.get('/', (req, res) => {
-  res.json({ message: `Server is running on port ${PORT}` });
+app.get("/", (req, res) => {
+	res.json({ message: `Server is running on port ${PORT}` });
 });
 
 // Set listen port for request
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
